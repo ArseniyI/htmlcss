a7909d85d959e0c19c8c92aa581969a0084f6e68
ArseniyI <ayakhymets@gmail.com>
2020-10-02 04:50:41 +0000

    Comment

--- scripts/site.js
+++ scripts/site.js
@@ -1,6 +1,70 @@
- function calculate_subtotal(index)
+ function calculateSubtotal(index)
  {
-    let quantity = document.getElementById("quantity" + index).value;
-    let price = document.getElementById("price" + index).innerText;
-    document.getElementById("subtotal" + index).innerText = quantity * price;
+    const quantity = document.getElementById("quantity" + index).value;
+    const price = document.getElementById("price" + index).innerText;
+    document.getElementById("subtotal" + index).innerText = round(quantity * price);
+    calculateTotal();
+ }
+
+ function calculateTotal()
+ {
+    let total = 0.0;
+
+    for(let index = 0; ; index++)
+    {
+        let subtotal = document.getElementById("subtotal" + index);
+
+        if(!subtotal)
+        {
+            break;
+        }
+
+        total += parseFloat(subtotal.innerText);
+    }
+
+    document.getElementById("total").innerText = round(total);
+ }
+
+ function calculateAll()
+ {
+    // Calculate subtotals
+    for(let index = 0; ; index++)
+    {
+        let subtotal = document.getElementById("subtotal" + index);
+
+        if(subtotal)
+        {
+            calculateSubtotal(index);
+        }
+        else
+        {
+            break;
+        }
+    }
+ }
+
+ function round(x)
+ {
+     return Math.round(x * 100) / 100.0;
+ }
+
+ function back()
+ {
+    history.back();
+ }
+
+ function clearSearch(control)
+ {
+    if(control.value === "Search...")
+     {
+         control.value = "";
+     }
+ }
+
+ function setSearch(control)
+ {
+    if(control.value === "")
+     {
+         control.value = "Search...";
+     }
  }
